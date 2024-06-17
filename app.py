0d439b8b182661bddcb5e9fe75167390daaf007f
Anderson <anderson@agenciaw3.digital>
2024-06-17 18:39:01 -0400

    change cad

--- app.py
+++ app.py
@@ -102,101 +102,44 @@ def create_table():
         ''')
 
 
-
-
-# Função para popular o banco de dados
-# def popular_banco_dados():
-#     # Popula o banco de dados com 300 alunos fictícios
-#     for i in range(300):
-#         nome = f'Aluno {i+1}'
-#         turno = random.choice(['Manhã', 'Tarde'])
-#         ano = random.choice(['Bercario-A', 'Bercario-B', 'Maternal-A', 'Maternal-B', 'Nivel-I-A', 'Nivel-I-B', 'Nivel-II-A', 'Nivel-II-B', 'Nivel-III-A', 'Nivel-III-B', '1-ano-A', '1-ano-B', '2-ano-A', '2-ano-B', '3-ano-A', '3-ano-B', '4-ano-A', '4-ano-B', '5-ano', '6-ano', '7-ano', '8-ano', '9-ano'])
-#         responsavel = f'Responsável {i+1}'
-#         contato = f'(00) 0000-000{i+1}'
-#         credito = f'{random.randint(0, 1000):.2f}'.replace('.', ',')
-
-#         # Insere o aluno no banco de dados
-#         with sqlite3.connect(DATABASE) as con:
-#             cur = con.cursor()
-#             cur.execute(
-#                 'INSERT INTO alunos (nome, turno, ano, responsavel, contato, credito) VALUES (?, ?, ?, ?, ?, ?)',
-#                 (nome, turno, ano, responsavel, contato, credito)
-#             )
-#             con.commit()
-
-#     # Popula o banco de dados com alguns dados de consumo e pagamentos para cada aluno
-#     for aluno_id in range(1, 301):
-#         # Popula o banco de dados com alguns dados de consumo para cada dia da semana
-#         for dia in ['segunda', 'terca', 'quarta', 'quinta', 'sexta']:
-#             consumo_diario = round(random.uniform(0, 20), 2)
-#             data_consumo = datetime.now() - timedelta(days=random.randint(0, 30))
-
-#             # Insere os dados de consumo no banco de dados
-#             with sqlite3.connect(DATABASE) as con:
-#                 cur = con.cursor()
-#                 cur.execute(
-#                     'INSERT INTO historico_consumo (aluno_id, data, valor, tipo_transacao) VALUES (?, ?, ?, ?)',
-#                     (aluno_id, data_consumo, consumo_diario, 'consumo')
-#                 )
-#                 con.commit()
-
-#         # Popula o banco de dados com alguns dados de pagamento
-#         for _ in range(random.randint(1, 5)):
-#             valor_pagamento = round(random.uniform(0, 100), 2)
-#             data_pagamento = datetime.now() - timedelta(days=random.randint(0, 30))
-
-#             # Insere os dados de pagamento no banco de dados
-#             with sqlite3.connect(DATABASE) as con:
-#                 cur = con.cursor()
-#                 cur.execute(
-#                     'INSERT INTO historico_consumo (aluno_id, data, valor, tipo_transacao) VALUES (?, ?, ?, ?)',
-#                     (aluno_id, data_pagamento, valor_pagamento, 'pagamento')
-#                 )
-#                 con.commit()
-
-# if __name__ == '__main__':
-#     create_table()
-#     popular_banco_dados()
-#     print('Banco de dados populado com sucesso!')
-
-
-
-
-
-
-
-
-
 # Rota para lidar com o envio do formulário
 @app.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
-  create_table()
-  if request.method == 'POST':
-    nome = request.form['nome']
-    turno = request.form['turno']
-    ano = request.form['ano']
-    responsavel = request.form['responsavel']
-    contato = request.form['contato']
-    credito = request.form['credito']
-    segunda = '0,00'
-    terca = '0,00'
-    quarta = '0,00'
-    quinta = '0,00'
-    sexta = '0,00'
+    create_table()
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        turno = request.form.get('turno')
+        ano = request.form.get('ano')
+        responsavel = request.form.get('responsavel')
+        contato = request.form.get('contato')
+        credito = request.form.get('credito')
 
-    # Inserir dados no banco de dados
-    with sqlite3.connect(DATABASE) as con:
-      cur = con.cursor()
-      cur.execute(
-          'INSERT INTO alunos (nome, turno, ano, responsavel, contato, credito, segunda, terca, quarta, quinta, sexta) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-          (nome, turno, ano, responsavel, contato, credito, segunda, terca,
-           quarta, quinta, sexta))
-      con.commit()
+        if not all([nome, turno, ano, responsavel, contato, credito]):
+            return 'Erro: todos os campos são obrigatórios', 400
 
-  # Redirecionar para a página de cadastro
-  return render_template('cadastro.html',
-                         success_message='Cadastro realizado com sucesso!')
+        segunda = '0,00'
+        terca = '0,00'
+        quarta = '0,00'
+        quinta = '0,00'
+        sexta = '0,00'
 
+        try:
+            # Inserir dados no banco de dados
+            with sqlite3.connect(DATABASE) as con:
+                cur = con.cursor()
+                cur.execute(
+                    'INSERT INTO alunos (nome, turno, ano, responsavel, contato, credito, segunda, terca, quarta, quinta, sexta) VALUES (?,?,?,?,?,?,?,?,?,?,?)',
+                    (nome, turno, ano, responsavel, contato, credito, segunda, terca,
+                     quarta, quinta, sexta))
+                con.commit()
+        except sqlite3.Error as e:
+            return f'Erro ao inserir dados no banco de dados: {e}', 500
+
+        # Redirecionar para a página de cadastro
+        return render_template('cadastro.html',
+                             success_message='Cadastro realizado com sucesso!')
+
+    return render_template('cadastro.html')
 
 # Rota para exibir a lista de alunos
 @app.route('/lista_alunos')
